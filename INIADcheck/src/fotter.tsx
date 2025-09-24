3e3e8201db267bba9677f1bf49ff46d493a51196
くらりゅ！ <s1f102501260@iniad.org>
2025-09-24 11:31:33 +0900

    fotter 位置調整

--- INIADcheck/src/fotter.tsx
+++ INIADcheck/src/fotter.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
     return (
         <>
-        <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+        <footer className="absolute bottom-0 w-full footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
             <aside>
                 <p>Copyright © {new Date().getFullYear()} - All right reserved by INIADcheck</p>
             </aside>
